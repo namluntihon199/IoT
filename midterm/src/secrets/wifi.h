8a0ed21cf3462157a6fffd70eb68fe223a640024
namluntihon199 <nguyentrunghieu_t66@hus.edu.vn>
2024-12-06 00:02:39 +0700

    Delete midterm/src/secrets/wifi.h

--- midterm/src/secrets/wifi.h
+++
@@ -1,9 +0,0 @@
-namespace WiFiSecrets
-{
-    const char *ssid = "Huong Giang";
-    const char *pass = "0974011612";
-    const char *echo_topic = "esp32/echo_test";
-    unsigned int publish_count = 0;
-    uint16_t keepAlive = 15;    // seconds (default is 15)
-    uint16_t socketTimeout = 5; // seconds (default is 15)
-}
