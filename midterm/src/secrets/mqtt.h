3205247e9833e6719eb84104a58387c0870ce064
namluntihon199 <nguyentrunghieu_t66@hus.edu.vn>
2024-12-06 00:02:28 +0700

    Delete midterm/src/secrets/mqtt.h

--- midterm/src/secrets/mqtt.h
+++
@@ -1,7 +0,0 @@
-namespace MQTT
-{
-    const char *username = "0823989969";
-    const char *pass = "Namluntihon2635-";
-    const char *broker = "0d8c1755ac044a4fb5b8348e60860a21.s1.eu.hivemq.cloud";
-    uint16_t port = 8883;
-}
